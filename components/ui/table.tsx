063190ed049406cd5c7c5dc548ddec5076a63214
Abdelaziz <abel-haj@student.1337.ma>
2025-02-25 12:18:11 +0000

    installed and used shadcn

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "@/lib/shadcn-utils"
+import { cn } from "@/lib/utils"
 
 const Table = React.forwardRef<
   HTMLTableElement,
@@ -11,7 +11,7 @@ const Table = React.forwardRef<
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
       {...props}
-    />
+    ></table>
   </div>
 ))
 Table.displayName = "Table"
